be477f722cc26c525bccde8942d48ea6b3383f3c
fyulai <2157791442@qq.com>
2025-08-28 21:19:38 +0800

    updates

--- fuzzers/Peach/shark2pit/subjects/bacnet/bacnet_build.sh
+++ fuzzers/Peach/shark2pit/subjects/bacnet/bacnet_build.sh
@@ -7,7 +7,7 @@ cd build
 cmake -DBUILD_EXAMPLES=ON \
     -DCMAKE_C_COMPILER=/root/pcguard-cov/afl-clang-fast \
     -DCMAKE_CXX_COMPILER=/root/pcguard-cov/afl-clang-fast++ \
-    -DCMAKE_C_FLAGS="-Wall -O1 -g -fsanitize=address,undefined -fno-omit-frame-pointer -fsanitize-coverage=trace-pc-guard" \
-    -DCMAKE_CXX_FLAGS="-Wall -O1 -g -fsanitize=address,undefined -fno-omit-frame-pointer -fsanitize-coverage=trace-pc-guard" \
+    -DCMAKE_C_FLAGS="-Wall -O1 -g -fno-omit-frame-pointer -fsanitize=memory -fsanitize-memory-track-origins -fsanitize-coverage=trace-pc-guard" \
+    -DCMAKE_CXX_FLAGS="-Wall -O1 -g -fno-omit-frame-pointer -fsanitize=memory -fsanitize-memory-track-origins -fsanitize-coverage=trace-pc-guard" \
     ..
 make
