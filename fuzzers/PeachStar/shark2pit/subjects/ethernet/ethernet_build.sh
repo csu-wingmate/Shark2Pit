51d6667d65d1b37bcd3ecef6145f8514eaa2b265
fyulai <2157791442@qq.com>
2025-09-15 16:43:42 +0800

    update

---
+++ fuzzers/PeachStar/shark2pit/subjects/ethernet/ethernet_build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euxo pipefail
+cd /root
+git clone https://github.com/EIPStackGroup/OpENer.git
+cd /root/OpENer/bin/posix
+cmake \
+-DBUILD_EXAMPLES=ON \
+-DCMAKE_C_COMPILER=/root/pcguard-cov/afl-clang-fast \
+-DCMAKE_CXX_COMPILER=/root/pcguard-cov/afl-clang-fast++ \
+-DOpENer_PLATFORM:STRING="POSIX" \          
+-DBUILD_SHARED_LIBS:BOOL=OFF \             
+-DCMAKE_BUILD_TYPE:STRING="Debug" \        
+-DCMAKE_C_FLAGS="-Wall -O1 -g -fsanitize=address,undefined -fno-omit-frame-pointer -fsanitize-coverage=trace-pc-guard" \
+-DCMAKE_CXX_FLAGS="-Wall -O1 -g -fsanitize=address,undefined -fno-omit-frame-pointer -fsanitize-coverage=trace-pc-guard" \
+../../source
+make -j$(nproc)
